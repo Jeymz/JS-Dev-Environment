05948ab4c2d88c0c9fea484a55e6100363f7a260
Jeymz <jeymzsimmons@live.com>
2017-06-12 20:35:25 -0400

    Feature Update
    Continueing with the feature updates

---
+++ buildScripts/testsetup.js
@@ -0,0 +1,7 @@
+// This file isn't transpiled, so must use CommonJS and ES5
+
+//register babel to transpile before out tests run.
+require('babel-register')();
+
+//Disable webpack features that mocha doesn't understand
+require.extensions[".css"] = function() {};
